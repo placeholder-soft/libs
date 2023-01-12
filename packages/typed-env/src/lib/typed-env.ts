16634e8bb7ff0c2d47ade276157fca93f5a1f319
loveyoubaby5120 <kaipan.1314.666@gmail.com>
2023-01-12 14:32:17 +0800

    feat: browser compatible

--- packages/typed-env/src/lib/typed-env.ts
+++ packages/typed-env/src/lib/typed-env.ts
@@ -1,17 +1,39 @@
-import { EnvBox } from "./env-box";
+import { Dict, EnvBox } from './env-box';
 
 /**
+ * default env source is process.env, if you want to use other env source, you can use injectGlobalEnv
+ */
+let __GLOBAL_ENV__ = process?.env ?? {};
+
+/**
+ * Set the global environment for the current process.
+ *
+ * @param env The environment to set.
+ * @example injectGlobalEnv(process.env) | injectGlobalEnv({"app_version": "0.0.1"})
+ *
+ */
+export const injectGlobalEnv = (env: Dict<string>) => {
+  __GLOBAL_ENV__ = env;
+};
+
+/**
+ *
  * @param key env key
  * @returns EnvBox
+ *
+ * @description support evn-name type check, default env source is process.env, if you want to use other env source, you can use injectGlobalEnv
+ *
  */
 export function typedEnv<T extends string>(key: T) {
-  return EnvBox.of(key);
+  return EnvBox.of(key, __GLOBAL_ENV__);
 }
 
 /**
  * @param key env key
  * @returns EnvBox
+ *
+ * @description can be any env-name, default env source is process.env, if you want to use other env source, you can use injectGlobalEnv
  */
 export function anyEnv(key: string) {
-  return EnvBox.of(key);
+  return EnvBox.of(key, __GLOBAL_ENV__);
 }
